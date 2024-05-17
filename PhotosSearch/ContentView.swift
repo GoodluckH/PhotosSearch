f0534cc9145c86f8e6cd4690f820b594563eb15b
Xipu Li <lixipu98@gmail.com>
2024-05-16 21:22:29 -0700

    Initial Commit

---
+++ PhotosSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotosSearch
+//
+//  Created by Xipu Li on 5/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotosSearch/PhotosSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosSearchApp.swift
+//  PhotosSearch
+//
+//  Created by Xipu Li on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotosSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
